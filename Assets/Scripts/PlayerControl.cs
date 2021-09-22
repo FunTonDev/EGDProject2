d4d018072a827d3dde3c1d0b550b9a8837b3cb55
FryGuy1348 <77214515+FryGuy1348@users.noreply.github.com>
2021-09-22 17:17:08 -0400

    Added stuff to player control

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    //The player body object
     PlayerControl player;
+    //The camera/head
     public FirstPersonCamera head;
+    //Stage manager
     public GameObject stagie;
-
+    //Bool to determine movement controls
+    public bool carMode;
+    //Speed the player moves at
     public float playerSpeed;
+    //The direction the player is facing in
     public Vector3 playerDirection = Vector3.zero;
 
     // Start is called before the first frame update
@@ -29,6 +35,7 @@ public class PlayerControl : MonoBehaviour
 
         transform.Translate(new Vector3(playerDirection.x, 0, playerDirection.z) * (playerSpeed * Time.deltaTime));
 
+        //If player presses the chaos button
         if (Input.GetButtonDown("Jump"))
         {
             stagie.GetComponent<StageManager>().chaos = !stagie.GetComponent<StageManager>().chaos;
